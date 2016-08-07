4afe1ddbef92fce5133de96484b11081f3da0adf
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2016-08-08 00:23:53 +0300

    addItem api
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- routes/client.js
+++ routes/client.js
@@ -5,20 +5,36 @@ var express =require('express');
 var router=express.Router();
 
 var assets=require('./models/asset');
+
 router.post('/addItem',function (req,res,next) {
     var assetData={
-    Address:req.header("Address"),
-    location:req.header("Location"),
-    country:req.header("Country"),
-    price:req.header("Location"),
-    owner:req.header("price")
+        Address:req.header("Address"),
+        location:req.header("Location"),
+        country:req.header("Country"),
+        price:req.header("Location"),
+        owner:req.header("price")
 
     };
 
     console.log("ADD ITEM");
     var asset=new assets(assetData);
-   // res.send(asset);
-   // res.send("WELL?!");
+    asset.save(function (err, a) {
+        if (err) {
+            res.json({
+                "error": err,
+                "status": false
+            });
+        }
+        else {
+            res.json({
+                "status": true,
+                "id": a._id
+            });
+        }
+    })
+    // res.send(asset);
+    // res.send("WELL?!");
 
     //Doha?should we use save here?
-})
+    // yes we should :D :D 
+});
